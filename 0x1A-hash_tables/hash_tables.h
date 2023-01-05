a16c6f37bf3a26e8511ce7da3a5ed7a612de8610
Psalmdave <miracleachumba@gmail.com>
2023-01-05 15:24:36 -0800

    6-hash_table_delete.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,11 +1,10 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-/* import liberys */
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
-/* define structs */
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -15,7 +14,6 @@
  * @next: A pointer to the next node of the List
  */
 typedef struct hash_node_s
-
 {
 char *key;
 char *value;
@@ -36,8 +34,6 @@ unsigned long int size;
 hash_node_t **array;
 } hash_table_t;
 
-/* my funtions proyect 0x1A */
-
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -46,4 +42,4 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-#endif
+#endif /* HASH_TABLES_H */
